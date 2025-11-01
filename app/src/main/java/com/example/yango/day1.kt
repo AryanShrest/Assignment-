296bc7008c051c2ef8dd8a475480eb4871f4974e
AryanShrest <aryanshrestha0307@gmail.com>
2025-11-01 12:49:31 +0545

    inital commit

---
+++ app/src/main/java/com/example/yango/day1.kt
@@ -0,0 +1,11 @@
+package com.example.yango
+
+fun main(){
+    print("hello")
+    println("word")
+
+    var name : String ="Aryan"
+    val age : Int = 18
+
+    print("my name is ${name.uppercase()} and age is $age")
+}
